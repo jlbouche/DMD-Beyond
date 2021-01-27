deed4e44b3f62ec3e25be5576996e8cf2579f345
JL <jennalbouche@gmail.com>
2021-01-27 00:55:07 -0800

    Deploy

--- config/database.js
+++ config/database.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  'mongodb://localhost:27017/dmdbeyond7',
-  { useNewUrlParser: true,
+mongoose.connect(process.env.DATABASE_URL, { 
+    useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false
